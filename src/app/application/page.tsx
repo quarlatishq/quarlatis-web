8d7634824a7fc3d5db377e4fb5d40dd061a75397
Lovepreet Singh <135840741+03-lovepreetSingh@users.noreply.github.com>
2025-06-05 04:08:37 +0530

    added the schema and added the apply

---
+++ src/app/application/page.tsx
@@ -0,0 +1,114 @@
+"use client"
+import { useState } from "react";
+import {
+  Navbar,
+  NavBody,
+  NavItems,
+  MobileNav,
+  NavbarLogo,
+  NavbarButton,
+  MobileNavHeader,
+  MobileNavToggle,
+  MobileNavMenu,
+} from "@/components/ui/resizable-navbar";
+
+import SignupFormDemo from "@/components/form";
+
+
+export default function NavbarDemo() {
+  const navItems = [
+    {
+      name: "Features",
+      link: "#features",
+    },
+    {
+      name: "Who are We",
+      link: "#about",
+    },
+    {
+      name: "Projects",
+      link: "projects",
+    },
+    {
+      name: "Carriers",
+      link: "#carriers",
+    },
+    {
+      name: "Contact",
+      link: "#contact",
+    },
+  ];
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  return (
+    <div className="dark bg-black relative w-full">
+      <Navbar>
+        {/* Desktop Navigation */}
+        <NavBody>
+          <NavbarLogo />
+          <NavItems items={navItems} />
+          <div className="flex items-center gap-4">
+            <NavbarButton variant="primary">Book a call</NavbarButton>
+          </div>
+        </NavBody>
+
+        {/* Mobile Navigation */}
+        <MobileNav>
+          <MobileNavHeader>
+            <NavbarLogo />
+            <MobileNavToggle
+              isOpen={isMobileMenuOpen}
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            />
+          </MobileNavHeader>
+
+          <MobileNavMenu
+            isOpen={isMobileMenuOpen}
+            onClose={() => setIsMobileMenuOpen(false)}
+          >
+            {navItems.map((item, idx) => (
+              <a
+                key={`mobile-link-${idx}`}
+                href={item.link}
+                onClick={() => setIsMobileMenuOpen(false)}
+                className="relative text-neutral-600 dark:text-neutral-300"
+              >
+                <span className="block">{item.name}</span>
+              </a>
+            ))}
+            <div className="flex w-full flex-col gap-4">
+              <NavbarButton
+                onClick={() => setIsMobileMenuOpen(false)}
+                variant="primary"
+                className="w-full"
+              >
+                Book a call
+              </NavbarButton>
+            </div>
+          </MobileNavMenu>
+        </MobileNav>
+      </Navbar>
+      {/* <ApplicationPage/> */}
+      <SignupFormDemo/>
+
+      {/* Navbar */}
+    </div>
+  );
+}
+
+
+export function ApplicationPage (){
+
+
+
+
+    return(
+
+        <div className="h-1000 text-white">
+
+            application CareersPage\
+
+        </div>
+    )
+}
---
+++ src/lib/prisma.ts
@@ -0,0 +1,5 @@
+import  PrismaClient  from '@prisma/client';
+ // @ts-ignore
+const prisma = new PrismaClient();
+
+export default prisma;
